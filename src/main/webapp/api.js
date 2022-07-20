d8ece3dccdfc8a960caef51853f06f8cb2efc633
vixxjy <ajorvictor48@gmail.com>
2022-07-20 22:04:09 +0100

    [consultation] updates made for the consultation lab test updates

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -2,4 +2,4 @@
 // export const url = '/api/'
 export const url =  'http://localhost:8282/api/v1/';
 export const apiUrl =  'http://localhost:8282/api/';
-export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjU4Mzg2NzI0fQ.bsu2V5U5EpolV1IHXs0F5f-3YjJuKAu6vglv4ZONqJg_eWRR_ni1KCrFlCIBkqYZn7kpkUV1sjLxE6OZd4wdQQ';
+export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjU4MzkzNjgyfQ.x4U3muSmmYrMGwudLCM42sUHxvFiWuFaB1wk1SjVZL51B9DJ3bRqmX1dtb1DqZ2QP1Ew_CVFIJrVtLxpxIB5hw';
